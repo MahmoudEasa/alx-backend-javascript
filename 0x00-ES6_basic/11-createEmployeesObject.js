c7e96acb9fefbf6e40cdd91dae74fd744d5a4392
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-02 13:09:36 +0000

    Task 11-createEmployeesObject.js

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,7 +1,22 @@
 #!/usr/bin/node
 
 /**
- * 
+ * Write a function named createEmployeesObject that will receive two arguments:
+ * @departmentName: String
+ * @employees: Array of Strings
+ *
+ * The function should return an object with the following format:
+ * {
+ *   $departmentName: [
+ *     $employees,
+ *   ],
+ * }
 */
 
-
+export default function createEmployeesObject(departmentName, employees) {
+  return (
+    {
+      [departmentName]: [...employees],
+    }
+  );
+}
