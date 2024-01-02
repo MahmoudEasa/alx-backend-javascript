0b001e7763ec1fced43b04c4a1941ccd980c1b9b
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-02 12:20:02 +0000

    Task 9-getFullBudget.js

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,7 +1,18 @@
 #!/usr/bin/node
 
 /**
- * 
+ * Rewrite getFullBudgetObject to use ES6 method properties in the fullBudget object
 */
 
+import getBudgetObject from './7-getBudgetObject.js';
 
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
+  };
+
+  return fullBudget;
+}
