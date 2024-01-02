c30a28bab5125c2b13b4f093a70fffe99b62cc91
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-02 12:08:46 +0000

    Task 8-getBudgetCurrentYear.js

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,7 +1,22 @@
 #!/usr/bin/node
 
 /**
- * 
+ * Rewrite the getBudgetForCurrentYear function to use ES6
+ * computed property names on the budget object
 */
 
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
 
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const currentYear = getCurrentYear();
+  const budget = {
+    [`income-${currentYear}`]: income,
+    [`gdp-${currentYear}`]: gdp,
+    [`capita-${currentYear}`]: capita,
+  };
+
+  return budget;
+}
