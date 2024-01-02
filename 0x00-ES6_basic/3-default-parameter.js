b50a57417b1add9f492c4dcdf5e031e24df4cacc
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-02 09:57:57 +0000

    Task 3-default-parameter.js

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
 
 /**
- * 
+ * Condense the internals of the following function to 1 line
+ * without changing the name of each function/variable.
 */
 
-
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
+}
