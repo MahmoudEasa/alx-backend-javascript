8848954177701c5ac2d4fd1046c41ca0e266c707
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-02 14:06:51 +0000

    Tasks 100-createIteratorObject.js & 101-iterateThroughObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,18 @@
 #!/usr/bin/node
 
 /**
- * 
+ * Write a function named createIteratorObject, that will take into argument
+ * a report Object created with the previous function createReportObject.
+ *
+ * This function will return an iterator to go through every employee in every department.
 */
 
+export default function createIteratorObject(report) {
+  const employees = report.allEmployees;
+  const result = [];
 
+  for (const i in employees)
+    for (const j in employees[i]) result.push(employees[i][j]);
+
+  return result;
+}
--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,13 @@
 #!/usr/bin/node
 
 /**
- * 
+ * Finally, write a function named iterateThroughObject.
+ * The function’s parameter reportWithIterator is the return
+ * value from createIteratorObject.
+ *
+ * It should return every employee name in a string, separated by |
 */
 
-
+export default function iterateThroughObject(reportWithIterator) {
+  return reportWithIterator.join(' | ');
+}
