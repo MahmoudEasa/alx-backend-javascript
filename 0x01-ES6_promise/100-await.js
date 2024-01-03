4efc2452d2432df58f358ea4a0329e6bbe847bf3
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-03 16:54:43 +0000

    Task 100-await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,1 +1,16 @@
+import { uploadPhoto, createUser } from './utils';
 
+const asyncUploadUser = async () => {
+  const resoult = { photo: null, user: null };
+
+  try {
+    const upload = await uploadPhoto();
+    const create = await createUser();
+    resoult.photo = upload;
+    resoult.user = create;
+
+    return (resoult);
+  } catch (err) { return (resoult); }
+};
+
+export default asyncUploadUser;
