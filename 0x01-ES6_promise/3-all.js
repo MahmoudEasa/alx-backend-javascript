3b42a08ab20c992779b6e42bc12ab7aa43f10ec3
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-03 14:51:43 +0000

    Fix Task 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,6 +3,6 @@ import { uploadPhoto, createUser } from './utils';
 export default async function handleProfileSignup() {
   try {
     const allPromises = await Promise.all([uploadPhoto(), createUser()]);
-    console.log(allPromises[0].body, allPromises[1].firstName, allPromises[1].lastName);
+    console.log(`${allPromises[0].body} ${allPromises[1].firstName} ${allPromises[1].lastName}`);
   } catch (err) { console.log('Signup system offline'); }
 }
