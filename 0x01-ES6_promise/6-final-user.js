1f93176d6ebb99637a043c938b5d272566056dd8
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-03 15:35:05 +0000

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,19 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
+const handleProfileSignup = async (firstName, lastName, fileName) => {
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  const allPromises = await Promise.all([signUpUserPromise, uploadPhotoPromise]);
+
+  const resoult = [
+    {
+      status: allPromises.status,
+      value: allPromises.status === 'fulfilled' ? allPromises.value : allPromises.reason,
+    },
+  ];
+  return (resoult);
+};
+
+export default handleProfileSignup;
