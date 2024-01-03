34bc987ffe588651504e3599f276b80171ce13d0
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-03 14:48:14 +0000

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,8 @@
+import { uploadPhoto, createUser } from './utils';
 
+export default async function handleProfileSignup() {
+  try {
+    const allPromises = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(allPromises[0].body, allPromises[1].firstName, allPromises[1].lastName);
+  } catch (err) { console.log('Signup system offline'); }
+}
