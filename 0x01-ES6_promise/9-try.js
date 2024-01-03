07c443735c29d2c705428e9b2d6383156b96f5bb
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-03 16:42:05 +0000

    Task 9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,14 @@
+const guardrail = (mathFunction) => {
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+    queue.push('Guardrail was processed');
+  } catch (err) {
+    queue.push(`Error: ${err.message}`);
+    queue.push('Guardrail was processed');
+  }
 
+  return (queue);
+};
+
+export default guardrail;
