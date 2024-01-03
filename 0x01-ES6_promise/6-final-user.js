f9f3d2e9b9f8e6d5b01bc5533d2fe31b02fa08fc
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-03 15:39:19 +0000

    Fix Task 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,18 +2,20 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 const handleProfileSignup = async (firstName, lastName, fileName) => {
-  const signUpUserPromise = signUpUser(firstName, lastName);
-  const uploadPhotoPromise = uploadPhoto(fileName);
+  try {
+    const signUpUserPromise = signUpUser(firstName, lastName);
+    const uploadPhotoPromise = uploadPhoto(fileName);
 
-  const allPromises = await Promise.all([signUpUserPromise, uploadPhotoPromise]);
+    const allPromises = await Promise.all([signUpUserPromise, uploadPhotoPromise]);
 
-  const resoult = [
-    {
-      status: allPromises.status,
-      value: allPromises.status === 'fulfilled' ? allPromises.value : allPromises.reason,
-    },
-  ];
-  return (resoult);
+    const resoult = [
+      {
+        status: allPromises.status,
+        value: allPromises.status === 'fulfilled' ? allPromises.value : allPromises.reason,
+      },
+    ];
+    return (resoult);
+  } catch (err) { return (err); }
 };
 
 export default handleProfileSignup;
