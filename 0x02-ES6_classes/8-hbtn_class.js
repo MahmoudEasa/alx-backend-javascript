2f9313328a49b722c700539ef1d446f5a8b61419
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-04 21:49:34 +0000

    Task 8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,1 +1,22 @@
+class HolbertonClass {
+  // size: Number
+  // location: String
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
 
+  toString() {
+    return (this._location);
+  }
+
+  toNumber() {
+    return (this._size);
+  }
+
+  valueOf() {
+    return (this._size);
+  }
+}
+
+export default HolbertonClass;
