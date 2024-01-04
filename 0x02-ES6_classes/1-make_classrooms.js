a08197a74872f2f17a16dabe21ef367a33331be6
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-04 16:46:52 +0000

    Task 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,1 +1,13 @@
+import ClassRoom from './0-classroom';
 
+const initializeRooms = () => {
+  const arr = [];
+
+  arr.push(new ClassRoom(19));
+  arr.push(new ClassRoom(20));
+  arr.push(new ClassRoom(34));
+
+  return (arr);
+};
+
+export default initializeRooms;
