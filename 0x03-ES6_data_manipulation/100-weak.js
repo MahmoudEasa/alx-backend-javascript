0e705a48b6cb75faf83379b2f24b8957e822082e
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-08 23:30:02 +0000

    Test Code Task 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,8 +1,39 @@
+export const weakMap = new WeakMap();
+
+const MAX_ENDPOINT = 5;
+
+/**
+ * Increase the count for the given endpoint in the weakMap
+ * and throw an error if the count exceeds the maximum limit.
+ * @param {object} endpoint - The API endpoint.
+ * @throws {Error} - If the count for the endpoint exceeds the maximum limit.
+ */
+export function queryAPI(endpoint) {
+  // If the endpoint is not present in the weakMap, initialize it with a count of 0.
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+
+  // Increment the count for the endpoint in the weakMap.
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+
+  // Check if the count for the endpoint exceeds the maximum limit.
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT) {
+    throw new Error('Position outside range');
+  }
+}
+
+
+
+
+
+
+
 /**
  * weakMap - instance of WeakMap
  */
 
-const weakMap = new WeakMap();
+//const weakMap = new WeakMap();
 
 /**
  * queryAPI - Track within the weakMap the number of times
@@ -10,12 +41,12 @@ const weakMap = new WeakMap();
  * @endPoint: Object
  */
 
-const queryAPI = (endPoint) => {
-  const endPointCount = (weakMap.get(endPoint) + 1) || 1;
+//const queryAPI = (endPoint) => {
+  //const endPointCount = (weakMap.get(endPoint) + 1) || 1;
 
-  if (endPointCount === 5) throw new Error('Endpoint load is high');
+  //if (endPointCount === 5) throw new Error('Endpoint load is high');
 
-  weakMap.set(endPoint, endPointCount);
-};
+  //weakMap.set(endPoint, endPointCount);
+//};
 
-export { weakMap, queryAPI };
+//export { weakMap, queryAPI };
