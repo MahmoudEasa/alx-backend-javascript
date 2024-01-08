494745982574afd237f84b737faf24e0d805e1d4
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-08 17:43:07 +0000

    Task 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,25 @@
+/**
+ * cleanSet - returns a string of all the set values
+ *   that start with a specific string (startString)
+ * @set: Set
+ * @startString: String
+ *
+ * Return: String
+ */
 
+const cleanSet = (set, startString) => {
+  const startStringLen = startString.length;
+
+  if (!startStringLen) return ('');
+
+  return (Array.from(set).map((el) => {
+    const start = el.slice(0, startStringLen);
+    if (start === startString) {
+      return (el.slice(startStringLen));
+    }
+
+    return (null);
+  }).filter((el) => el)).join('-');
+};
+
+export default cleanSet;
