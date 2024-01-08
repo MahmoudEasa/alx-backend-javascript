3750e83ba23a96f870c5c01d4c30e7ca5fe30852
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-08 15:43:21 +0000

    Task 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,20 @@
+/**
+ * createInt8TypedArray - returns a new ArrayBuffer
+ *    with an Int8 value at a specific position
+ * @length: Number
+ * @position: Number
+ * @value: Number
+ *
+ * Return: a new ArrayBuffer
+*/
 
+const createInt8TypedArray = (length, position, value) => {
+  if (position >= length) throw new Error('Position outside range');
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Uint8Array(buffer);
+  int8Array[position] = value;
+
+  return (buffer);
+};
+
+export default createInt8TypedArray;
