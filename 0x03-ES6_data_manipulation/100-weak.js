d136e3bbcb646b33a502be8a2ff4be9fdb1c10db
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-08 21:24:00 +0000

    Task 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,1 +1,20 @@
+/**
+ * weakMap - instance of WeakMap
+ */
 
+const weakMap = new WeakMap();
+
+/**
+ * queryAPI - Track within the weakMap the number of times
+ * queryAPI is called for each endpoint
+ * @endPoint: Object
+ */
+
+const queryAPI = (endPoint) => {
+  const endPointCount = (weakMap.get(endPoint) + 1) || 1;
+  weakMap.set(endPoint, endPointCount);
+
+  if (endPointCount === 5) throw new Error('Endpoint load is high');
+};
+
+export { weakMap, queryAPI };
