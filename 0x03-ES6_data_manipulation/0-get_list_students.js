3ece12c2214a9c9d6c5abe770ba0e0bb6cc902d3
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-08 12:15:54 +0000

    Task 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,1 +1,13 @@
+// id: Number
+// firstName: String
+// location: String
 
+const getListStudents = () => (
+  [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ]
+);
+
+export default getListStudents;
