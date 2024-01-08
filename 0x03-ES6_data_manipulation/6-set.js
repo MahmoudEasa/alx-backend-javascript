fdbb43c55fd65377d05a50ababe44fcc5af1000e
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-08 16:00:10 +0000

    Task 6-set.js

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,1 +1,10 @@
+/**
+ * setFromArray - returns a Set from an array
+ * @array: Array, of any kind of element
+ *
+ * Return: a Set from an array
+ */
 
+const setFromArray = (array) => new Set(array);
+
+export default setFromArray;
