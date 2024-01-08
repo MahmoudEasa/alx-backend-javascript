a7fbd7f460ca30a48a9b50f7fde0d0c8b4193272
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-08 17:15:22 +0000

    Task 7-has_array_values.js

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,1 +1,28 @@
+/**
+ * hasValuesFromArray - returns a boolean if all the elements
+ *   in the array exist within the set
+ * @set: Set
+ * @array: Array
+ *
+ * Return: Boolean
+ */
 
+const hasValuesFromArray = (set, array) => {
+  let isExist = false;
+
+  array.map((el) => {
+    for (const val of set) {
+      if (el === val) {
+        isExist = true;
+        break;
+      }
+      isExist = false;
+    }
+
+    return (el);
+  });
+
+  return (isExist);
+};
+
+export default hasValuesFromArray;
