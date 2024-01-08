a7a3ea97791c1efc5e1de165fd9d561268044050
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-08 19:36:54 +0000

    Task 10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,1 +1,18 @@
+/**
+ * updateUniqueItems - That returns an updated map
+ *   for all items with initial quantity at 1
+ * @map: Map
+ *
+ * Return: Updated map
+ */
 
+const updateUniqueItems = (map) => {
+  if (!(map instanceof Map)) throw new Error('Cannot process');
+
+  return (Array.from(map).map((el) => {
+    if (map.get(el[0]) === 1) map.set(el[0], 100);
+    return (el);
+  }));
+};
+
+export default updateUniqueItems;
