28861b9a25ffdc5849e81e5e139b82ff50715747
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-08 15:11:20 +0000

    Task 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,16 @@
+// city: String
+// newGrades: Array of “grade” objects
 
+const updateStudentGradeByCity = (listOfStudents, city, newGrades) => (
+  listOfStudents.filter((el) => el.location === city)
+    .map((el) => {
+      const student = el;
+      student.grade = 'N/A';
+      return newGrades.map((item) => {
+        if (student.id === item.studentId) student.grade = item.grade;
+        return (student);
+      })[0];
+    })
+);
+
+export default updateStudentGradeByCity;
