1bc2f17a998f724f1b60ebc136c68c10e7badfa8
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-08 12:31:46 +0000

    Task 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,1 +1,9 @@
+const getListStudentIds = (listOfObjs) => {
+  if (!Array.isArray(listOfObjs)) return ([]);
 
+  return (
+    listOfObjs.map((el) => (el.id))
+  );
+};
+
+export default getListStudentIds;
