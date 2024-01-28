3eb23778c92b042a455e15fae36bf314554f53d1
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-28 17:49:29 +0000

    Tasks 5 and 6

--- 0x04-TypeScript/task_2/js/main.js
+++ 0x04-TypeScript/task_2/js/main.js
@@ -1,9 +1,62 @@
 ;
-var director1 = {
-    firstName: 'John',
-    lastName: 'Doe',
-    location: 'London',
-    fullTimeEmployee: true,
-    numberOfReports: 17
+;
+;
+var Director = /** @class */ (function () {
+    function Director() {
+    }
+    Director.prototype.workFromHome = function () {
+        return ("Working from home");
+    };
+    Director.prototype.getCoffeeBreak = function () {
+        return ("Getting a coffee break");
+    };
+    Director.prototype.workDirectorTasks = function () {
+        return ("Getting to director tasks");
+    };
+    return Director;
+}());
+var Teacher = /** @class */ (function () {
+    function Teacher() {
+    }
+    Teacher.prototype.workFromHome = function () {
+        return ("Cannot work from home");
+    };
+    Teacher.prototype.getCoffeeBreak = function () {
+        return ("Cannot have a break");
+    };
+    Teacher.prototype.workTeacherTasks = function () {
+        return ("Getting to work");
+    };
+    return Teacher;
+}());
+var createEmployee = function (salary) {
+    if ((typeof salary === "number") && (salary < 500))
+        return (new Teacher());
+    else
+        return (new Director());
 };
-console.log(director1);
+var t1 = createEmployee(200);
+var d1 = createEmployee(1000);
+var d2 = createEmployee("$500");
+console.log(t1);
+console.log(t1.workFromHome());
+console.log(t1.getCoffeeBreak());
+if (t1 instanceof Teacher)
+    console.log(t1.workTeacherTasks());
+console.log(d1);
+console.log(d1.workFromHome());
+console.log(d1.getCoffeeBreak());
+if (d1 instanceof Director)
+    console.log(d1.workDirectorTasks());
+console.log(d2);
+var isDirector = function (employee) {
+    return (employee instanceof Director);
+};
+var executeWork = function (employee) {
+    if (isDirector(employee))
+        console.log(employee.workDirectorTasks());
+    else
+        console.log(employee.workTeacherTasks());
+};
+executeWork(createEmployee(200));
+executeWork(createEmployee(1000));
--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,22 +1,78 @@
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [key: string]: any;
+interface GeneralInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
 };
 
-interface Directors extends Teacher {
-  numberOfReports: number;
+
+interface DirectorInterface extends GeneralInterface {
+  workDirectorTasks(): string;
+};
+
+
+interface TeacherInterface extends GeneralInterface {
+  workTeacherTasks(): string;
+};
+
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return ("Working from home");
+  }
+
+  getCoffeeBreak(): string {
+    return ("Getting a coffee break");
+  }
+
+  workDirectorTasks(): string {
+    return ("Getting to director tasks");
+  }
 }
 
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17
-};
 
-console.log(director1);
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return ("Cannot work from home");
+  }
+
+  getCoffeeBreak(): string {
+    return ("Cannot have a break");
+  }
+
+  workTeacherTasks(): string {
+    return ("Getting to work");
+  }
+}
+
+const createEmployee = (salary: number | string): Teacher | Director => {
+  if ((typeof salary === "number") && (salary < 500)) return (new Teacher());
+  else return (new Director());
+}
+
+const t1 = createEmployee(200);
+const d1 = createEmployee(1000);
+const d2 = createEmployee("$500");
+
+console.log(t1);
+console.log(t1.workFromHome());
+console.log(t1.getCoffeeBreak());
+if (t1 instanceof Teacher) console.log(t1.workTeacherTasks());
+console.log(d1);
+console.log(d1.workFromHome());
+console.log(d1.getCoffeeBreak());
+if (d1 instanceof Director) console.log(d1.workDirectorTasks());
+console.log(d2);
+
+
+const isDirector = (employee: Director | Teacher): employee is Director => {
+  return (employee instanceof Director);
+}
+
+const executeWork = (employee) => {
+  if (isDirector(employee))
+    console.log(employee.workDirectorTasks());
+  else
+    console.log(employee.workTeacherTasks());
+}
+
+executeWork(createEmployee(200));
+executeWork(createEmployee(1000));
