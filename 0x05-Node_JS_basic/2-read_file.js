4381f0cbd41ddd8136b85897e7f5638199d65d04
MahmoudEasa <mu01011422865@gmail.com>
2024-02-27 20:27:51 +0200

    Fix Task 2. Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,37 +1,41 @@
 const fs = require('fs');
 
-const countStudents = async (path) => {
+const countStudents = (path) => {
   if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
   }
 
   try {
-    const data = fs.readFileSync(path, 'utf-8').split('\n').splice(1);
+    const file_content = fs.readFileSync(path, 'utf-8');
+    let data = null;
+    if (file_content.length) data = file_content.split('\n').splice(1);
     let data_len = 0
     const field_obj = {};
-    data.forEach((e) => {
-      const line = e.split(',');
-      const line_len = line.length;
-      if (line_len > 0) {
-        data_len++;
-        const filed = line[line_len - 1];
-        const first_name = line[0];
-        if (filed in field_obj) {
-          field_obj[filed].data.push(first_name);
-          field_obj[filed].count++;
+    if (data) {
+      data.forEach((e) => {
+        const line = e.split(',');
+        const line_len = line.length;
+        if (line_len > 0) {
+          data_len++;
+          const filed = line[line_len - 1];
+          const first_name = line[0];
+          if (filed in field_obj) {
+            field_obj[filed].data.push(first_name);
+            field_obj[filed].count++;
+          }
+          else field_obj[filed] = {
+            data: [first_name],
+            count: 1
+          };
         }
-        else field_obj[filed] = {
-          data: [first_name],
-          count: 1
-        };
+      })
+  
+      console.log(`Number of students: ${data_len}`);
+      for (field in field_obj) {
+        const data = field_obj[field].data.join(', ');
+        const f_len = field_obj[field].count;
+        console.log(`Number of students in ${field}: ${f_len}. List: ${data}`);
       }
-    })
-
-    console.log(`Number of students: ${data_len}`);
-    for (field in field_obj) {
-      const data = field_obj[field].data.join(', ');
-      const f_len = field_obj[field].count;
-      console.log(`Number of students in ${field}: ${f_len}. List: ${data}`);
     }
   } catch (err) {
     console.log(err);
