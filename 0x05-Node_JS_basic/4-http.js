710849bb8ec7b1dde8e9e1261c9822e7e23c931f
MahmoudEasa <mu01011422865@gmail.com>
2024-02-28 00:10:39 +0200

    Task 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,8 @@
-const http = require('node:http');
+const http = require('http');
 
 const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
