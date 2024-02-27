c58a8a2eed1c6e5f3aa1eb8c405bfd6138f25fc4
MahmoudEasa <mu01011422865@gmail.com>
2024-02-27 16:45:02 +0200

    Fix Task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
 const readline = require("readline");
-r = readline.createInterface({
+const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout,
 });
 
 console.log("Welcome to Holberton School, what is your name?");
-r.on("line", (input) => {
-	if (input.trim() === "exit") {
-		console.log("This important software is now closing");
-		process.exit(0);
-	} else console.log(`Your name is: ${input}`);
+rl.on("line", (input) => {
+	if (input === "exit") rl.close();
+	else console.log(`Your name is: ${input}`);
 });
+
+rl.on("close", () => console.log("This important software is now closing"));
