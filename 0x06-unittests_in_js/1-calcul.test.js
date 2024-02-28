a1d917637ba6b8cb54fc3d0321b4fd2188556c63
MahmoudEasa <mu01011422865@gmail.com>
2024-02-29 00:23:11 +0200

    Task 1

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,1 +1,20 @@
+const calculateNumber = require('./1-calcul');
+const assert = require('assert');
 
+describe('calculateNumber', () => {
+	it('Should round a and b and return the SUM it (1.4, 4.5 = 6)', () => {
+		assert.strictEqual(calculateNumber("SUM", 1.4, 4.5), 6);
+	});
+
+	it('Should round a and b and return the SUBTRACT it (1.4, 4.5 = -4)', () => {
+		assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+	});
+
+	it('Should round a and b and return the DIVIDE it (1.4, 4.5 = 0.2)', () => {
+		assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+	});
+
+	it('Should round a and b and return the DIVIDE it (1.4, 0 = Error)', () => {
+		assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+	});
+});
