77d1d3b5026c3f2d7f3481a9ced27f0e3380b04e
MahmoudEasa <mu01011422865@gmail.com>
2024-02-28 22:42:27 +0200

    Task 1

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,6 +7,5 @@ describe("calculateNumber", () => {
 		assert.equal(calculateNumber(1, 3.7), 5);
 		assert.equal(calculateNumber(1.2, 3.7), 5);
 		assert.equal(calculateNumber(1.5, 3.7), 6);
-		assert.equal(calculateNumber(), NaN);
 	});
 });
