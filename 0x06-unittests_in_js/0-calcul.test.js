605471bf9d4901965f2ea53b64776a42f973608b
MahmoudEasa <mu01011422865@gmail.com>
2024-02-28 23:11:32 +0200

    Fix Task 1

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,14 +4,33 @@ const assert = require('assert');
 describe('calculateNumber', () => {
 	it('Should round a and b and return the sum of it', () => {
 		assert.equal(calculateNumber(1, 3), 4);
+	});
+
+	it('Should round a and b and return the sum of it', () => {
 		assert.equal(calculateNumber(1, 3.7), 5);
+	});
+
+	it('Should round a and b and return the sum of it', () => {
 		assert.equal(calculateNumber(1.2, 3.7), 5);
+	});
+
+	it('Should round a and b and return the sum of it', () => {
 		assert.equal(calculateNumber(1.5, 3.7), 6);
 	});
+
 	it('Should round a and b and return the sum of it', () => {
 		assert.equal(calculateNumber(), NaN);
+	});
+
+	it('Should round a and b and return the sum of it', () => {
 		assert.equal(calculateNumber(0, 0), 0);
+	});
+
+	it('Should round a and b and return the sum of it', () => {
 		assert.equal(calculateNumber('', 4), 4);
+	});
+
+	it('Should round a and b and return the sum of it', () => {
 		assert.equal(calculateNumber('4', 4), 8);
 	});
 });
