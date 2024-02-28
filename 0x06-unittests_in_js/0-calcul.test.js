e1af79f5a860524418a9a71d412d3abd618a0138
MahmoudEasa <mu01011422865@gmail.com>
2024-02-28 23:04:31 +0200

    Task 1

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,6 +7,8 @@ describe("calculateNumber", () => {
 		assert.equal(calculateNumber(1, 3.7), 5);
 		assert.equal(calculateNumber(1.2, 3.7), 5);
 		assert.equal(calculateNumber(1.5, 3.7), 6);
+	});
+	it("Should round a and b and return the sum of it", () => {
 		assert.equal(calculateNumber(), NaN);
 		assert.equal(calculateNumber(0, 0), 0);
 		assert.equal(calculateNumber("", 4), 4);
