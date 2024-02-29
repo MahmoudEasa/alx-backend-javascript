cc3b1cb5b2ddcd17497a7e0d17102943965d46a9
MahmoudEasa <mu01011422865@gmail.com>
2024-02-29 21:04:16 +0200

    Task 10

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,1 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 7865;
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+	res.send("Welcome to the payment system");
+});
+
+app.get("/cart/:id(\\d+)", (req, res) => {
+    res.send(`Payment methods for cart ${req.params.id}`);
+});
+
+app.post("/login", (req, res) => {
+    res.send(`Welcome ${req.body.userName}`);
+});
+
+app.get("/available_payments", (req, res) => {
+	const data = {
+		payment_methods: {
+			credit_cards: true,
+			paypal: false,
+		},
+	};
+	res.json(data);
+});
+
+app.listen(port, () => {
+	console.log(`API available on localhost port ${port}`);
+});
+
+module.exports = app;
 
--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,1 +1,58 @@
+const { expect } = require("chai");
+const request = require('request');
+
+const url = "http://localhost:7865";
+
+describe("Index page", () => {
+	it("Index Page Testing...", (done) => {
+		request(`${url}`, (err, res) => {
+			expect(res.statusCode).to.equal(200);
+			expect(res.body).to.equal("Welcome to the payment system");
+			done();
+		})
+	});
+
+	it("Cart Page Testing...", (done) => {
+		request(`${url}/cart/12`, (err, res) => {
+			expect(res.statusCode).to.equal(200);
+			expect(res.body).to.equal("Payment methods for cart 12");
+            done();
+		})
+	});
+    
+	it("Cart Page Testing...", (done) => {
+        request(`${url}/cart/hello`, (err, res) => {
+            expect(res.statusCode).to.equal(404);
+            done();
+		})
+	});
+    
+	it("login Testing...", (done) => {
+        const reqData = {
+            url: `${url}/login`,
+            json: {
+                userName: 'Mahmoud Easa',
+            }
+        }
+        request.post(reqData, (err, res) => {
+            expect(res.statusCode).to.equal(200);
+            expect(res.body).to.equal("Welcome Mahmoud Easa");
+            done();
+		})
+	});
+    
+	it("Available Payments Testing...", (done) => {
+        request(`${url}/available_payments`, (err, res) => {
+            expect(res.statusCode).to.equal(200);
+            const data = {
+                payment_methods: {
+                    credit_cards: true,
+                    paypal: false,
+                },
+            };
+            expect(JSON.parse(res.body)).to.deep.equal(data);
+            done();
+		})
+	});
+});
 
