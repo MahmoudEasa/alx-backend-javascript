ed424ce39340e12ccf08661a0b9014473eba70cd
MahmoudEasa <mu01011422865@gmail.com>
2024-02-29 19:55:32 +0200

    ga

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,1 +1,12 @@
+const { expect } = require("chai");
+const request = require('request');
 
+describe("Index page", () => {
+	it("Testing...", (done) => {
+		request("http://localhost:7865", (err, res) => {
+			expect(res.statusCode).to.equal(200);
+			expect(res.body).to.equal("Welcome to the payment system");
+			done();
+		})
+	});
+});
