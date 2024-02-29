ffa5cd2d8cd2f929eb74c0858a63f6afbf9ff93b
MahmoudEasa <mu01011422865@gmail.com>
2024-02-29 02:32:08 +0200

    Task 3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,1 +1,16 @@
+const sinon = require("sinon");
+const { expect } = require("chai");
+const sendPaymentRequestToApi = require("./3-payment");
+const Utils = require('./utils');
 
+describe("sendPaymentRequestToApi", () => {
+    it("sendPaymentRequestToApi", () => {
+    const calculateNumberSpy = sinon.spy(Utils);
+
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumberSpy.sendPaymentRequestToApi('SUM', 100, 20)).to.be.true;
+
+    // Restore the original method to avoid interference with other tests
+    calculateNumberSpy.restore();
+    });
+});
--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,1 +1,13 @@
+class Utils {
+	static calculateNumber(type, a, b) {
+		if (type === "SUM") return Math.round(a) + Math.round(b);
+		if (type === "SUBTRACT") return Math.round(a) - Math.round(b);
+		if (type === "DIVIDE") {
+			const roundB = Math.round(b);
+			if (roundB) return Math.round(a) / roundB;
+			return "Error";
+		}
+	}
+}
 
+module.exports = Utils;
