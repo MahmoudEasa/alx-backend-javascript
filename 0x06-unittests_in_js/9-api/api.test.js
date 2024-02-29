4561a4c4c25aee7e070b925b0e80c07538f9f504
MahmoudEasa <mu01011422865@gmail.com>
2024-02-29 20:20:05 +0200

    Task 9

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,1 +1,23 @@
+const { expect } = require("chai");
+const request = require('request');
 
+describe("Index page", () => {
+	it("Index Page Testing...", (done) => {
+		request("http://localhost:7865", (err, res) => {
+			expect(res.statusCode).to.equal(200);
+			expect(res.body).to.equal("Welcome to the payment system");
+			done();
+		})
+	});
+
+	it("Cart Page...", (done) => {
+		request("http://localhost:7865/cart/12", (err, res) => {
+			expect(res.statusCode).to.equal(200);
+		})
+
+		request("http://localhost:7865/cart/hello", (err, res) => {
+			expect(res.statusCode).to.equal(404);
+		})
+        done();
+	});
+});
