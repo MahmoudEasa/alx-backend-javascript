62fa9f820a222c8c226c20bdf88d0427bad98998
MahmoudEasa <mu01011422865@gmail.com>
2024-02-29 18:22:34 +0200

    Task 4

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,7 +1,6 @@
 const Utils = require('./utils');
 
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
-    const utils = new Utils();
     const sum = Utils.calculateNumber("SUM", totalAmount, totalShipping)
     console.log(`The total is: ${sum}`);
 }
--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,1 +1,20 @@
+const sinon = require("sinon");
+const { expect } = require("chai");
+const sendPaymentRequestToApi = require("./4-payment");
+const Utils = require('./utils');
+
+describe("sendPaymentRequestToApi", () => {
+    it("sendPaymentRequestToApi", () => {
+    const calculateNumberSpy = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const consoleLogSpy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+
+    // Restore the original method to avoid interference with other tests
+    calculateNumberSpy.restore();
+    consoleLogSpy.restore();
+    });
+});
 
